9bf68bc7afceeaaf8577784f19972fae2f6d02d1
Akhtar Ramadhan Putra <153797480+Akhtar1212@users.noreply.github.com>
2024-03-06 05:00:11 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -12,8 +12,8 @@ def load_data():
 df = load_data()
 
 # Cetak nama kolom yang ada dalam DataFrame
-st.write("weathersit")
-st.write(df["weathersit"])
+st.write("dteday")
+st.write(df["dteday"])
 
 # Data Wrangling
 df['dteday'] = pd.to_datetime(df['dteday'])
